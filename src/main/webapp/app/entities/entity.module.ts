66af8a507a3485a3b90e325e8886b3d390fd8347
sagehornung <sagehornung@gmail.com>
2017-06-24 22:46:35 -0700

    Contact Page Added -- Going to make into a static page

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -3,6 +3,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BluecreekBlogModule } from './blog/blog.module';
 import { BluecreekEntryModule } from './entry/entry.module';
 import { BluecreekTagModule } from './tag/tag.module';
+import { BluecreekContactModule } from './contact/contact.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
@@ -10,6 +11,7 @@ import { BluecreekTagModule } from './tag/tag.module';
         BluecreekBlogModule,
         BluecreekEntryModule,
         BluecreekTagModule,
+        BluecreekContactModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
